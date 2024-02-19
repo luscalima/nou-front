838a15fe948897a7353139b93f2aa32ab8a536bf
LuscaLima <lusca.lima@outlook.com>
2024-02-19 17:15:54 -0300

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>title</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam voluptatibus, ut odio error
+      itaque odit officiis, consectetur qui harum excepturi assumenda dolores! Officia quo non
+      nostrum autem commodi facere tempora?
+    </p>
+  </main>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
