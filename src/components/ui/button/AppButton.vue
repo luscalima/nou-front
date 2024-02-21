7db0b063803667fa5ff41270299bf208d6e6d40a
LuscaLima <lusca.lima@outlook.com>
2024-02-21 18:01:40 -0300

    feat: Add app button component

---
+++ src/components/ui/button/AppButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { type ButtonVariants, buttonVariants } from '.'
+import { cn } from '@/lib/utils'
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  as?: string
+  class?: HTMLAttributes['class']
+  rounded?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+  rounded: false
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size, rounded }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
