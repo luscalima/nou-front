a77b263c8233009be326e022d34f5665cc22288d
LuscaLima <lusca.lima@outlook.com>
2024-02-28 21:10:53 -0300

    feat: Add support to data loading

--- src/components/ui/data-table/DataTable.vue
+++ src/components/ui/data-table/DataTable.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts" generic="Data, Value">
+import { computed } from 'vue'
 import { FlexRender, getCoreRowModel, useVueTable, type ColumnDef } from '@tanstack/vue-table'
 import {
   Table as AppTable,
@@ -8,11 +9,19 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table'
+import { PhCircleNotch } from '@phosphor-icons/vue'
 
-const props = defineProps<{
-  columns: ColumnDef<Data, Value>[]
-  data: Data[]
-}>()
+const props = withDefaults(
+  defineProps<{
+    columns: ColumnDef<Data, Value>[]
+    data?: Data[]
+    loading: boolean
+  }>(),
+  {
+    data: () => [],
+    loading: false
+  }
+)
 
 const table = useVueTable({
   get data() {
@@ -23,34 +32,46 @@ const table = useVueTable({
   },
   getCoreRowModel: getCoreRowModel()
 })
+
+const hasAnyData = computed(() => table.getRowModel().rows?.length > 0 && !props.loading)
 </script>
 
 <template>
-  <div class="border rounded-md">
-    <AppTable>
-      <TableHeader>
-        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-          <TableHead v-for="header in headerGroup.headers" :key="header.id">
-            <FlexRender
-              v-if="!header.isPlaceholder"
-              :render="header.column.columnDef.header"
-              :props="header.getContext()"
-            />
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <template v-if="table.getRowModel().rows?.length">
-          <TableRow v-for="row in table.getRowModel().rows" :key="row.id">
-            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-              <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+  <div>
+    <div class="mb-4">
+      <slot name="actions"> </slot>
+    </div>
+    <div class="border rounded-md">
+      <AppTable>
+        <TableHeader>
+          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+            <TableHead v-for="header in headerGroup.headers" :key="header.id">
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <template v-if="hasAnyData">
+            <TableRow v-for="row in table.getRowModel().rows" :key="row.id">
+              <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+                <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+              </TableCell>
+            </TableRow>
+          </template>
+          <TableRow v-else>
+            <TableCell v-if="loading" :colspan="columns.length" class="h-24">
+              <PhCircleNotch size="24" class="animate-spin mx-auto" />
+            </TableCell>
+            <TableCell v-else :colspan="columns.length" class="h-24 text-center">
+              No data.
             </TableCell>
           </TableRow>
-        </template>
-        <TableRow v-else>
-          <TableCell col-span="{columns.length}" class="h-24 text-center"> No results. </TableCell>
-        </TableRow>
-      </TableBody>
-    </AppTable>
+        </TableBody>
+      </AppTable>
+    </div>
   </div>
 </template>
--- src/components/ui/table/TableCell.vue
+++ src/components/ui/table/TableCell.vue
@@ -4,18 +4,12 @@ import { cn } from '@/lib/utils'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
+  colspan?: number
 }>()
 </script>
 
 <template>
-  <td
-    :class="
-      cn(
-        'p-4 align-middle [&:has([role=checkbox])]:pr-0',
-        props.class,
-      )
-    "
-  >
+  <td :colspan="colspan" :class="cn('p-4 align-middle [&:has([role=checkbox])]:pr-0', props.class)">
     <slot />
   </td>
 </template>
